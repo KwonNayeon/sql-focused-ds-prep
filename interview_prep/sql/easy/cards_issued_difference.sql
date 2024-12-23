9da2ad8d98ea3df8fcafd9358941cd65e1fa0a42
Nayeon <nayeonkn0330@gmail.com>
2024-12-23 13:11:57 +0900

    Create cards_issued_difference.sql

---
+++ interview_prep/sql/easy/cards_issued_difference.sql
@@ -0,0 +1,12 @@
+-- ============================================================
+-- Cards Issued Difference
+-- Filename: cards_issued_difference.sql
+-- ============================================================
+
+SELECT
+  card_name,
+  max(issued_amount) - min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by difference desc
+;
