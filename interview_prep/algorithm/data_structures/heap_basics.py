595a173b0c3ae11ba5e6c1efad95fd72fd70316f
Nayeon <nayeonkn0330@gmail.com>
2025-06-30 14:56:21 -0400

    Add heap_basics.md

---
+++ interview_prep/algorithm/data_structures/heap_basics.py
@@ -0,0 +1,37 @@
+# 모듈 임포트
+from heapq import heappush, heappop
+
+# 최소 힙 생성
+heap = []
+
+# 힙에 원소 추가
+from heapq import heappush
+
+heap = []
+
+heappush(heap, 4)
+heappush(heap, 1)
+heappush(heap, 7)
+heappush(heap, 3)
+heappush(heap, 8)
+heappush(heap, 5)
+
+print(heap)
+
+# 힙에서 원소 삭제
+
+
+# 최소값 삭제하지 않고 얻기
+
+
+# 기존 리스트를 힙으로 변환
+
+
+# 응용 - 최대 힙
+
+
+# 응용 - n번째 최소값/최대값
+
+
+# 응용 - 힙 정렬
+
