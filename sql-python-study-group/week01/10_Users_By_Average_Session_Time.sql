f6e018da7e54393b9497882ae4d5426f60c8e252
Nayeon <nayeonkn0330@gmail.com>
2025-11-08 09:13:17 -0500

    Solve Users By Average Session Time (SQL)

---
+++ sql-python-study-group/week01/10_Users_By_Average_Session_Time.sql
@@ -0,0 +1,20 @@
+with daily_sessions as (
+    select
+        user_id,
+        EXTRACT(DAY FROM timestamp) as day,
+        max(case when action = 'page_load' then timestamp end) as max_load,
+        min(case when action = 'page_exit' then timestamp end) as min_exit
+    from facebook_web_log
+    group by 1,2
+)
+select
+    user_id,
+    avg(min_exit - max_load) as avg_session_duration
+from daily_sessions
+where min_exit is not null
+    and max_load is not null
+group by user_id
+;
+
+-- Review Notes:
+-- 2025-11-08: 다시 풀어보기
