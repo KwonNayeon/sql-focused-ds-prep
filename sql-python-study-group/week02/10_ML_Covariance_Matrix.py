3071d316a06c78d8a02a17ab83e33c4c7ff04e43
Nayeon <nayeonkn0330@gmail.com>
2025-11-17 16:20:07 -0500

    Solve Calculate Covariance Matrix

---
+++ sql-python-study-group/week02/10_ML_Covariance_Matrix.py
@@ -0,0 +1,23 @@
+# 직접 구현
+def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
+    # Define dimension variables - col, row
+    n_features, n_obs = len(vectors), len(vectors[0])
+    # Initialize our return: covariance matrix
+    cov_matrix = [[0 for _ in range(n_features)] for _ in range(n_features)]
+    # N*N
+    # Pre-calculate means
+    means = [sum(feature)/n_obs for feature in vectors]
+    # Populate the return matrix
+    for i in range(n_features):
+        for j in range(n_features):
+            cov = sum(((vectors[i][k] - means[i])*(vectors[j][k] - means[j])) for k in range(n_obs))/(n_obs-1)
+            cov_matrix[i][j] = cov
+            cov_matrix[j][i] = cov
+
+    return cov_matrix
+
+# numpy 활용
+import numpy as np
+
+def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
+    return np.cov(vectors)
