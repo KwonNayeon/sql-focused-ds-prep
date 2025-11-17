94a027f805b3c51adb3493b371e39252309ec3b9
Nayeon <nayeonkn0330@gmail.com>
2025-11-16 19:33:36 -0500

    Solve Linear Regression Using Normal Equation

---
+++ sql-python-study-group/week02/08_ML_LinearRegression_NormalEquation.py
@@ -0,0 +1,11 @@
+import numpy as np
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+	# Your code here, make sure to round
+    X = np.array(X)
+    y = np.array(y).reshape(-1, 1)
+    X_T = X.T
+    # (XᵀX)⁻¹ Xᵀy 계산 — np.linalg.inv는 행렬의 역행렬을 구함
+    theta = np.linalg.inv(X_T.dot(X)).dot(X_T).dot(y)
+    # 다차원 배열을 1차원으로 펼침 -> numpy 배열을 파이썬 리스트로 변환
+    theta = np.round(theta, 4).flatten().tolist()
+	return theta
