5499322544d08ac0a3b0a4cfc1d39bcfb889dec9
Nayeon <nayeonkn0330@gmail.com>
2025-12-19 13:23:14 -0500

    Solve Day 20 problem

---
+++ advent_2025/day20.sql
@@ -0,0 +1,17 @@
+with cte as (
+  select
+    *,
+    extract(year from purchased_at) as year
+  from transactions
+  where is_online_order = 1
+)
+select
+  year,
+  sum(case when shipping_method = 'Standard' then 1 else 0 end) 
+    + sum(case when is_returned = 1 then 1 else 0 end) as standard,
+  sum(case when shipping_method = 'Express' then 1 else 0 end) as express,
+  sum(case when shipping_method = 'Overnight' then 1 else 0 end) as overnight
+from cte
+group by year
+order by year
+;
