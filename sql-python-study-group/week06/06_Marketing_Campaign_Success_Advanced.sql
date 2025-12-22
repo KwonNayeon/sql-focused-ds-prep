40cc02f60a5e486d150d04eb1677cd55f38ffcc5
Nayeon <nayeonkn0330@gmail.com>
2025-12-22 10:49:54 -0500

    refactor: simplify query

--- sql-python-study-group/week06/06_Marketing_Campaign_Success_Advanced.sql
+++ sql-python-study-group/week06/06_Marketing_Campaign_Success_Advanced.sql
@@ -1,3 +1,4 @@
+-- 처음 쿼리
 WITH cte1 AS (
     SELECT
         user_id,
@@ -30,5 +31,20 @@ FROM (
     SELECT user_id, product_id FROM cte2
 ) new_products
 ;
+
+-- 개선된 쿼리
+-- 1. dense rank는 같은 날짜에 여러 개 구매했을 경우 같은 순위로 처리하는 것을 이용
+-- 2. 같은 제품을 다시 구매할 경우, product_rn이 2가 됨
+with cte as (
+    select
+        user_id, created_at, product_id,
+        dense_rank() over (partition by user_id order by created_at) as data_rn,
+        dense_rank() over (partition by user_id, product_id order by created_at) as product_rn
+    from marketing_campaign
+)
+select count(distinct user_id) as user_cnt
+from cte
+where data_rn > 1 and product_rn = 1
+
 -- Review Notes:
 -- 2025-12-15: 복습하기
