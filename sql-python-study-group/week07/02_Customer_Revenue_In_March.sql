9a7a25c12b17f7dfce65fdef7a60724d77cb1613
Nayeon <nayeonkn0330@gmail.com>
2026-01-01 08:38:56 -0500

    Solve Customer Revenue In March (SQL)

---
+++ sql-python-study-group/week07/02_Customer_Revenue_In_March.sql
@@ -0,0 +1,9 @@
+-- Customer Revenue In March
+select
+    cust_id,
+    sum(total_order_cost) as revenue
+from orders
+where extract(month from order_date) = 3
+group by cust_id
+order by sum(total_order_cost) desc
+;
