e147ba0f5116a8014cb0e70ba60f611e73bf7407
Nayeon <nayeonkn0330@gmail.com>
2026-02-05 15:24:18 -0500

    Solve Find Students At Median Writing

---
+++ sql-python-study-group/week07/10_Find_Students_At_Median_Writing.sql
@@ -0,0 +1,12 @@
+-- Find Students At Median Writing
+-- PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY price) AS median_price
+with cte as (
+    select
+        percentile_cont(0.5) within group (order by sat_writing) as median_writing
+    from sat_scores
+)
+select student_id
+from sat_scores s
+join cte c
+on s.sat_writing = c.median_writing
+;
