cea376b0d1de61890b58de8af1a5e04682704a4a
Nayeon <nayeonkn0330@gmail.com>
2026-02-06 16:40:53 -0500

    Solve Find Students At Median Writing

---
+++ sql-python-study-group/week07/10_Find_Students_At_Median_Writing.py
@@ -0,0 +1,10 @@
+# Find Students At Median Writing
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = sat_scores.copy()
+
+median_writing = df['sat_writing'].median()
+
+result = df[df['sat_writing'] == median_writing]['student_id']
