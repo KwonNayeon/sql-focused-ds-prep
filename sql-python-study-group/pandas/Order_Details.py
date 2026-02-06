1af5ecc3a11f7291872232fc2ce465c048b4aec7
Nayeon <nayeonkn0330@gmail.com>
2026-02-06 15:58:33 -0500

    Solve a Joins & Merging problem

---
+++ sql-python-study-group/pandas/Order_Details.py
@@ -0,0 +1,12 @@
+# Joins & Merging
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+customers.head()
+
+merged = customers.merge(orders, left_on='id', right_on='cust_id')
+
+filtered = merged[merged['first_name'].isin(['Jill', 'Eva'])]
+
+result = filtered[['first_name', 'order_date', 'order_details', 'total_order_cost']]
