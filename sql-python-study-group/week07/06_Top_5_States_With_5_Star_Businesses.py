d8922223132787b684f7c69e4feda194dbee34d6
Nayeon <nayeonkn0330@gmail.com>
2026-02-09 12:54:05 -0500

    Solve Top 5 States With 5 Star Businesses (Pandas)

---
+++ sql-python-study-group/week07/06_Top_5_States_With_5_Star_Businesses.py
@@ -0,0 +1,13 @@
+# Top 5 States With 5 Star Businesses
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = yelp_business.copy()
+
+df = (df.query("stars <= 5")
+        .groupby('state')['business_id'].count().reset_index(name='n_businesses'))
+        
+df['rank'] = df['n_businesses'].rank(method='dense', ascending=False)
+
+result = df.sort_values('rank', ascending=True)
