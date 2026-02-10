ed86fa12ac0ab6473e58e91bbd31c5c463ba8534
Nayeon <nayeonkn0330@gmail.com>
2026-02-10 14:53:41 -0500

    Solve a Window Functions problem

---
+++ sql-python-study-group/pandas/Top_Percentile_Fraud.py
@@ -0,0 +1,10 @@
+# Window Functions
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = fraud_score.copy()
+
+df['p95'] = df.groupby('state')['fraud_score'].transform(lambda x: x.quantile(0.95))
+
+result = df.query("fraud_score >= p95").drop(columns='p95')
