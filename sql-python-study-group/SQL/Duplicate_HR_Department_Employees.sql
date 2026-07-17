79e1ea3afcec39a1fcfe5906bdb4d0524a07c1fa
Nayeon <nayeonkn0330@gmail.com>
2026-07-17 11:51:49 +0900

    Add query explanations

--- sql-python-study-group/SQL/Duplicate_HR_Department_Employees.sql
+++ sql-python-study-group/SQL/Duplicate_HR_Department_Employees.sql
@@ -12,3 +12,6 @@ select
 from worker
 where department='HR'
 ;
+
+-- union: 두 결과를 합친 후 중복 제거
+-- union all: 단순히 두 결과를 합침, 중복을 제거하지 않음
--- sql-python-study-group/SQL/Maximum_of_Two Numbers.sql
+++ sql-python-study-group/SQL/Maximum_of_Two Numbers.sql
@@ -5,3 +5,6 @@ select
 from deloitte_numbers a
 cross join deloitte_numbers b
 ;
+
+-- cross join: 두 테이블의 모든 행 조합을 만듦(Cartesian product)
+-- greatest: 한 행 안의 칼럼끼리 비교
