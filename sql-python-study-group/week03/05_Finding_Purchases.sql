014474e6ccb5dae650678bc18ee07c0bb3025698
Nayeon <nayeonkn0330@gmail.com>
2026-07-21 15:23:57 +0900

    Review Finding Purchases

--- sql-python-study-group/week03/05_Finding_Purchases.sql
+++ sql-python-study-group/week03/05_Finding_Purchases.sql
@@ -14,5 +14,20 @@ with_lag as (
 select
     distinct user_id
 from with_lag
-where (day - previous_day) between 1 and 7      -- same-day purchases는 제외
+where (day - previous_day) between 1 and 7      -- same day purchases는 제외
 ;
+
+-- 다른 풀이
+with cte as (
+    select
+        *,
+        lag(created_at) over (partition by user_id order by created_at) as previous_day
+    from amazon_transactions
+)
+select
+    distinct user_id
+from cte
+where (created_at-previous_day) < 8
+and created_at!=previous_day
+;
+
