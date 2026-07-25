b06af644fae86bd5b989721016c26249fc35c281
Nayeon <nayeonkn0330@gmail.com>
2026-07-25 16:05:11 +0900

    Review Find the percentage of shipable orders

--- sql-python-study-group/week05/09_Find_The_Percentage_Of_Shipable_Orders.sql
+++ sql-python-study-group/week05/09_Find_The_Percentage_Of_Shipable_Orders.sql
@@ -1,7 +1,9 @@
 -- Find the percentage of shipable orders
 select
-    (count(case when c.address is not null then 1 end) * 1.0 / count(*) * 1.0)*100 as shipable_orders
+    (count(case when c.address is not null then 1 end) * 1.0 / count(*))*100 as shipable_orders
 from orders o
 join customers c
 on o.cust_id = c.id
 ;
+-- Review Notes:
+-- 2026-07-25: 복습, 비율 계산 시 나눗셈 전에 한쪽을 실수형으로 만들어 주는 것 잊지 말기
