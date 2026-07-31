9c81fd41a11fc2c5465b38085e0a3e9e268a3cc3
Nayeon <nayeonkn0330@gmail.com>
2026-07-31 12:45:50 +0900

    Review Second Highest Salary

--- interview_prep/sql/phase1/medium/second_highest_salary.sql
+++ interview_prep/sql/phase1/medium/second_highest_salary.sql
@@ -1,37 +1,15 @@
--- 첫 번째 풀이
-WITH data AS (
-   SELECT DISTINCT salary
-   FROM employee
-   ORDER BY salary DESC
-   LIMIT 1 OFFSET 1
-)
-SELECT * 
-FROM data;
-
--- 2025/02/10 풀이
-with data as (
-  SELECT
-    dense_rank() over (order by salary desc) as rn,
-    salary
-  from employee
-)
-SELECT
-  salary
-from data
-where rn = 2
-;
-
--- 2026/07/27 풀이
 select
-    salary
+  salary
 from (
-    select
-        *,
-        rank() over (order by salary desc) as rk
-    from employee
-)
+  select
+    *,
+    rank() over(order by salary desc) as rk
+  from employee
+) ranked
 where rk = 2
 ;
 
 -- Review Notes:
 -- 2026-07-27: 복습
+-- 2026-07-31: 복습
+
