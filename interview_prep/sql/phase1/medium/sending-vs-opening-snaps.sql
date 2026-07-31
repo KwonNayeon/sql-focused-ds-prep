b916057c03eb61d6d3ebfdb575e5b836ba6b9816
Nayeon <nayeonkn0330@gmail.com>
2026-07-31 12:53:52 +0900

    Review Sending vs. Opening Snaps

--- interview_prep/sql/phase1/medium/sending-vs-opening-snaps.sql
+++ interview_prep/sql/phase1/medium/sending-vs-opening-snaps.sql
@@ -14,3 +14,17 @@ select
 from data
 group by age_bucket
 ;
+
+-- 2026/07/31 풀이
+SELECT
+  b.age_bucket,
+  round(sum(case when a.activity_type = 'send' then time_spent else 0 end)*1.0/(sum(case when a.activity_type = 'send' then time_spent else 0 end)+sum(case when a.activity_type = 'open' then time_spent else 0 end))*100,2) as send_perc,
+  round(sum(case when a.activity_type = 'open' then time_spent else 0 end)*1.0/(sum(case when a.activity_type = 'send' then time_spent else 0 end)+sum(case when a.activity_type = 'open' then time_spent else 0 end))*100,2) as open_perc
+FROM activities a
+join age_breakdown b
+on a.user_id = b.user_id
+group by b.age_bucket
+;
+
+-- Review Notes:
+-- 2026-07-31: 복습
