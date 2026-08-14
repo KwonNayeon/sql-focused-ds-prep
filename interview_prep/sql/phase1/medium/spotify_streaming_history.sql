3730fe9ed291ad18fd1a54dbd60c27c3867f26fb
Nayeon <nayeonkn0330@gmail.com>
2026-08-14 11:57:03 +0900

    Solve Spotify Streaming History

---
+++ interview_prep/sql/phase1/medium/spotify_streaming_history.sql
@@ -0,0 +1,14 @@
+select
+  COALESCE(a.user_id, b.user_id) as user_id,
+  COALESCE(a.song_id, b.song_id) as song_id,
+  COALESCE(a.song_plays, 0) + count(b.listen_time) as song_plays
+from songs_history a 
+full join songs_weekly b
+on a.user_id = b.user_id and a.song_id = b.song_id
+where date(b.listen_time) <= '2022-08-04' or b.listen_time is null
+group by COALESCE(a.user_id, b.user_id), COALESCE(a.song_id, b.song_id), a.song_plays
+order by song_plays desc
+;
+
+-- Review Notes:
+-- 2026-08-14: join 조건, null값 처리가 까다로운 문제 (한 테이블에만 존재하는 데이터 처리 때문), 복습하기
