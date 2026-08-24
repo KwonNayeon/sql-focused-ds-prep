940b8b705ccf1798edda45e6f673e880b0f86610
Nayeon <nayeonkn0330@gmail.com>
2026-08-24 16:02:27 +0900

    Review Patient Support Analysis (Part 2)

--- interview_prep/sql/phase2/Patient_Support_Analysis_2.sql
+++ interview_prep/sql/phase2/Patient_Support_Analysis_2.sql
@@ -6,3 +6,4 @@ FROM callers
 -- Review Notes:
 -- 2026-08-21:
 -- call_category in ('n/a', NULL) 이런 식으로 in절 안에 NULL을 넣으면 call_category in ('n/a')와 같이 동작함. 그러므로 NULL 처리를 명시적으로 해주는 것이 중요
+-- 2026-08-24: Null을 확인할 땐 is null, Null이 아닌 것을 확인할 땐 is not null을 사용해야 함. = Null은 틀린 문법
