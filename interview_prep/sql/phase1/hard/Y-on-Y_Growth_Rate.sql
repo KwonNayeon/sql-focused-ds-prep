7aad65099f8c578346cc791d9d1ec50926849273
Nayeon <nayeonkn0330@gmail.com>
2026-08-26 09:44:05 +0900

    Review Y-on-Y Growth Rate

--- interview_prep/sql/phase1/hard/Y-on-Y_Growth_Rate.sql
+++ interview_prep/sql/phase1/hard/Y-on-Y_Growth_Rate.sql
@@ -1,3 +1,4 @@
+-- 첫 번째 풀이
 with cte1 as (
   SELECT
     extract(year from transaction_date) as year,
@@ -18,5 +19,24 @@ select
 from cte2
 ;
 
+-- 개선된 풀이
+with yearly_spend_cte as (
+  SELECT
+    extract(year from transaction_date) as year,
+    product_id,
+    spend as curr_year_spend,
+    lag(spend) over (partition by product_id order by product_id, extract(year from transaction_date)) as prev_year_spend
+  FROM user_transactions
+)
+select
+  year,
+  product_id,
+  curr_year_spend,
+  prev_year_spend,
+  round((curr_year_spend-prev_year_spend)*1.0/prev_year_spend*100,2) as yoy_rate
+from yearly_spend_cte
+;
+
 -- Review Notes:
 -- 2026-08-25: 다른 풀이 복습하기
+-- 2026-08-26: 푸는 방법 개선
