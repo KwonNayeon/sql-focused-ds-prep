4995d008bc7c338ad8e655aa1e18b602f01eb798
Nayeon <nayeonkn0330@gmail.com>
2026-08-28 14:39:57 +0900

    Review Maximize Prime Item Inventory

--- interview_prep/sql/phase1/hard/Maximize_Prime_Item_Inventory.sql
+++ interview_prep/sql/phase1/hard/Maximize_Prime_Item_Inventory.sql
@@ -1,28 +1,39 @@
-with summary as (
-  SELECT
-    item_type,
-    sum(square_footage) as total_sqft,
-    count(item_id) as item_count
-  FROM inventory
-  group by item_type
-),
-prime_occupied_area as (
+with prime_batch as (
   select
-    item_type,
-    total_sqft,
-    floor(500000/total_sqft) as prime_item_batch_count,
-    floor((500000/total_sqft) * item_count) as prime_item_count
-  from summary
+    sum(square_footage) as batch_sqft,
+    count(item_id) as items_per_batch
+  from inventory
   where item_type = 'prime_eligible'
+),
+nonprime_batch as (
+  select
+    sum(square_footage) as batch_sqft,
+    count(item_id) as items_per_batch
+  from inventory
+  where item_type = 'not_prime'
+),
+calc as (
+  select
+    p.batch_sqft as prime_sqft,
+    p.items_per_batch as prime_items,
+    n.batch_sqft as nonprime_sqft,
+    n.items_per_batch as nonprime_items,
+    -- non-prime 배치 1개 자리는 미리 빼놓고 prime 배치 수를 최대화
+    floor((500000 - n.batch_sqft) / p.batch_sqft) as prime_batch_count
+  from prime_batch p, nonprime_batch n
 )
-select
-  item_type,
-  case when item_type = 'prime_eligible' then (floor(500000/total_sqft) * item_count)
-    when item_type = 'not_prime' then floor((500000 - (SELECT FLOOR(500000/total_sqft) * total_sqft from prime_occupied_area)) / total_sqft) * item_count
-  end as item_count
-from summary
-order by item_type desc
-;
+select 'prime_eligible' as item_type,
+       prime_batch_count * prime_items as item_count
+from calc
+
+union all
+
+select 'not_prime' as item_type,
+       floor((500000 - prime_batch_count * prime_sqft) / nonprime_sqft) * nonprime_items as item_count
+from calc
+
+order by item_type desc;
 
 -- Review Notes:
 -- 2026-08-27: 내일 복습하기
+-- 2026-08-28: 풀이는 어느정도 이해 되는데, 혼자 풀기는 어려울듯
