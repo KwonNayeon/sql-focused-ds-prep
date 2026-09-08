f10f14b83439c43935a3b107155805d0f1bfaa99
Nayeon <nayeonkn0330@gmail.com>
2026-09-08 09:51:47 +0900

    Review Final Account Balance

--- interview_prep/sql/phase2/final_account_balance.sql
+++ interview_prep/sql/phase2/final_account_balance.sql
@@ -1,3 +1,4 @@
+-- 풀이 1
 -- CASE문으로 Deposit은 +, Withdrawal은 -로 변환 후 SUM
 SELECT
   account_id,
@@ -7,4 +8,19 @@ SELECT
   END) as final_balance
 from transactions
 group by account_id
+;
+
+-- 풀이 2
+with cte as (
+  SELECT
+    account_id,
+    sum(case when transaction_type = 'Deposit' then amount else 0 end) as deposit,
+    sum(case when transaction_type = 'Withdrawal' then amount else 0 end) as withdrawal
+  FROM transactions
+  group by account_id
+)
+select
+  account_id,
+  (deposit - withdrawal) as final_balance
+from cte
 ;
