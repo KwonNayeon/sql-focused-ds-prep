17db9be0e3e89784262ed47225173e8c07a5fdd6
Nayeon <nayeonkn0330@gmail.com>
2026-09-10 18:38:34 +0900

    Review Cards Issued Difference

--- interview_prep/sql/phase2/Cards_Issued_Difference.sql
+++ interview_prep/sql/phase2/Cards_Issued_Difference.sql
@@ -1,3 +1,4 @@
+-- 풀이 1
 with data as(
   select
     card_name,
@@ -10,5 +11,14 @@ select
   card_name,
   max - min as difference
 from data
+order by difference desc
+;
+
+-- 풀이 2
+SELECT
+  card_name,
+  max(issued_amount) - min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
 order by difference desc
 ;
