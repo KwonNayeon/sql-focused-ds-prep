3c4e92e225148100675cfbfb1571e662d81e4cf7
Nayeon <nayeonkn0330@gmail.com>
2026-09-11 12:47:38 +0900

    Review Compressed Mean

--- interview_prep/sql/phase2/Compressed_Mean.sql
+++ interview_prep/sql/phase2/Compressed_Mean.sql
@@ -1,3 +1,4 @@
+-- 풀이 1
 -- 정수 나눗셈을 방지하기 위해서 ::numeric 사용해야 함
 with data as (
   SELECT
@@ -8,4 +9,11 @@ with data as (
 select 
   round(total_items::numeric / total_orders, 1) as mean
 from data
+;
+
+-- 풀이 2
+-- 정수 나눗셈을 방지하기 위해서 분자를 미리 소수로 변환
+select
+  round(sum(item_count::DECIMAL*order_occurrences) / sum(order_occurrences), 1) as mean
+from items_per_order
 ;
